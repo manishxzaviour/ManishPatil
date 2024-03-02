53367fec85fdb8d6074f77966e55dbc14da20019
Manish Patil <manishpatil7058@gmail.com>
2024-03-02 22:11:26 +0530

    book pageFlip done

--- scene2.js
+++ scene2.js
@@ -26,6 +26,49 @@ export class Scene2{
 		this.contextDiv.style.color='rgb(150,150,150)';
 		this.contextDiv.style.filter='drop-shadow(2px 2px rgba(0,0,0,0.5))';
 		this.contextDiv.style.zIndex=5;
+		
+		this.pageFlip=`	<div class="pageContainer">
+				    <div class="page" id="first">
+					<div class="back">
+					    <div class="outer">
+						<div class="content">
+							0000
+						</div>
+					    </div>
+					</div>
+				    </div>
+				    <div class="page" id="second">
+					<div class="front">
+					    <div class="outer">
+						<div class="content">
+							1111
+						</div>
+					    </div>
+					</div>
+					<div class="back" id="third">
+					    <div class="outer">
+						<div class="content">
+							2222
+						</div>
+					    </div>
+					</div>
+				    </div>
+				    <div class="page" id="fourth">
+					<div class="front">
+					    <div class="outer">
+						<div class="content">
+							3333
+						</div>
+					    </div>
+					</div>
+				    </div>
+
+				    <div id="prev"></div>
+				    <div id="select"></div>
+				    <div id="next"></div>
+				</div>
+			`;
+		this.pageSelected=0;
 
 		this.book.addEventListener('mouseover',function(e){
 			this.book.addEventListener('mousemove',function(e){
@@ -53,75 +96,61 @@ export class Scene2{
 
 			//tympanus.net> bookflip
 			this.bookWrapper=document.createElement('div');
-			this.bookWrapper.innerHTML=`
-				<div class="pageContainer">
-				    <div class="page" id="first">
-					<div class="back">
-					    <div class="outer">
-						<div class="content">
-							<img src="./res/scene2/1.png">
-						</div>
-					    </div>
-					</div>
-				    </div>
-				    <div class="page" id="second">
-					<div class="front">
-					    <div class="outer">
-						<div class="content">
-						    <img src="./res/scene2/1.png">
-						</div>
-					    </div>
-					</div>
-					<div class="back" id="third">
-					    <div class="outer">
-						<div class="content">
-							    <img src="./res/scene2/2.png">
-						</div>
-					    </div>
-					</div>
-				    </div>
-				    <div class="page" id="fourth">
-					<div class="front">
-					    <div class="outer">
-						<div class="content">
-						   <img src="./res/scene2/2.png">
-						    
-						</div>
-					    </div>
-					</div>
-				    </div>
-
-				    <div id="prev"></div>
-				    <div id="select"></div>
-				    <div id="next"></div>
-				</div>
-			`;
+			let inner=this.pageFlip.replace('0000','');
+			inner=inner.replace('1111','<img src="./res/scene2/bookFlat.png">');
+			inner=inner.replace('2222','<img src="./res/scene2/1.png">');
+			this.bookWrapper.innerHTML=inner.replace('3333','<img src="./res/scene2/1.png">');
 			this.wrapper.append(this.bookWrapper);
-			let prev = document.getElementById("prev");
-			let next = document.getElementById("next");
-			let select = document.getElementById("select");
+			this.wrapper.style.transition='0.5s';
+			this.wrapper.style.left=window.innerWidth/2-this.wrapper.clientWidth/2.5+'px';
 
-			prev.addEventListener("click", prevImg.bind(this));
-			next.addEventListener("click", nextImg.bind(this));
-			select.addEventListener("click", sel.bind(this));
+			setTimeout(()=>{
+				this.wrapper.style.transition='0.2s';
+				this.wrapper.style.left=window.innerWidth/2-this.wrapper.clientWidth/2+'px';
+				let second=document.getElementById('second');
+				second.style.msTransform = "rotateY(-180deg)";
+				second.style.webkitTransform = "rotateY(-180deg)";
+				second.style.transform = "rotateY(-180deg)";
+				setTimeout(function(){
+					inner=this.pageFlip.replace('0000','<img src="./res/scene2/1.png">');
+					inner=inner.replace('1111','<img src="./res/scene2/1.png">');
+					inner=inner.replace('2222','<img src="./res/scene2/2.png">');
+					inner=inner.replace('3333','<img src="./res/scene2/2.png">');
+					this.bookWrapper.innerHTML=inner;
+
+					let prev = document.getElementById("prev");
+					let next = document.getElementById("next");
+					let select = document.getElementById("select");
+
+					prev.addEventListener("click", prevImg.bind(this));
+					next.addEventListener("click", nextImg.bind(this));
+					select.addEventListener("click", sel.bind(this));
 
 
 
-			let second = document.getElementById('second');
-			let fourth= document.getElementById('fourth');
+					let second = document.getElementById('second');
+					let fourth= document.getElementById('fourth');
 
-			function prevImg() {
-				    second.style.msTransform = "rotateY(0deg)";
-				    second.style.webkitTransform = "rotateY(0deg)";
-				    second.style.transform = "rotateY(0deg)";
-			}
-			function nextImg() {
-				    second.style.msTransform = "rotateY(-180deg)";
-				    second.style.webkitTransform = "rotateY(-180deg)";
-				    second.style.transform = "rotateY(-180deg)";
-			}
-			function sel(){
-			}
+					function prevImg() {
+						    second.style.msTransform = "rotateY(0deg)";
+						    second.style.webkitTransform = "rotateY(0deg)";
+						    second.style.transform = "rotateY(0deg)";
+						this.pageSelected=0;
+					}
+					function nextImg() {
+						    second.style.msTransform = "rotateY(-180deg)";
+						    second.style.webkitTransform = "rotateY(-180deg)";
+						    second.style.transform = "rotateY(-180deg)";
+						this.pageSelected=1;
+					}
+					function sel(){
+						this.nextScene=this.pageSelected;
+						this.done=true;
+						this.wrapper.style.filter='blur(10px)';
+					}
+				}.bind(this),700);
+			},500);
+
 		}.bind(this);
 
 		this.downloadButton.addEventListener('mouseover',function(e){
